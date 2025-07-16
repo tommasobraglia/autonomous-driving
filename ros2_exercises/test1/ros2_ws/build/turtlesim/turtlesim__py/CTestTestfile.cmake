8e3264b04e38b95e312eea63ab44b58706ae7e16
Tommaso Braglia <63156696+tommasobraglia@users.noreply.github.com>
2025-07-16 20:06:33 +0200

    Delete ros2_exercises/test1/ros2_ws directory

--- ros2_exercises/test1/ros2_ws/build/turtlesim/turtlesim__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tbraglia/ros2_exercises/test1/ros2_ws/build/turtlesim/turtlesim__py
-# Build directory: /home/tbraglia/ros2_exercises/test1/ros2_ws/build/turtlesim/turtlesim__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
